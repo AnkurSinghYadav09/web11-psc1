40cd3b241c58403b8544385b34227126ea14a99e
hilalhussa31 <100183431+AnkurSinghYadav09@users.noreply.github.com>
2023-05-02 15:05:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AddProduct from "./component/AddProduct";
+import ProductsItems from "./component/ProductsItems";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setToggle(!toggle)}>
+        {toggle ? "add product" : "show Products"}
+      </button>
+      {toggle ? <ProductsItems /> : <AddProduct />}
     </div>
   );
 }
